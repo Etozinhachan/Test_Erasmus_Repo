359fe1a96b97aaca5562fc1ad70b6a490f6b08b2
Etozinhachan <116160881+Etozinhachan@users.noreply.github.com>
2024-01-30 19:50:07 +0000

    trying to make the dashboard table scrollable

--- obj/Debug/net8.0/testingStuff.AssemblyInfo.cs
+++ obj/Debug/net8.0/testingStuff.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testingStuff")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d33dbff64d686654b9042881a20de4e31ed6e2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b71e0b15eea0b1ebdfe498b15a2d4f25e95e221f")]
 [assembly: System.Reflection.AssemblyProductAttribute("testingStuff")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testingStuff")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

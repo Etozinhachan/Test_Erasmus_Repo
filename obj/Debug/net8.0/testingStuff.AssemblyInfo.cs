8a7aa58765e27be85a0b43a1a41ebdae6850027a
Etozinhachan <etochandev@gmail.com>
2024-02-13 17:53:22 +0000

    added stuff

--- obj/Debug/net8.0/testingStuff.AssemblyInfo.cs
+++ obj/Debug/net8.0/testingStuff.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testingStuff")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a5e7f50edf1aebf51690d95c1aca4e5ee240bc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("testingStuff")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testingStuff")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
